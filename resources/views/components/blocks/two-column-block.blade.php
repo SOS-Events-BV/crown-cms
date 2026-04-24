6f2452a313b5b2c563da9cf396af147eb8156f57
Steven Roest <stevenroest@hotmail.com>
2026-04-24 13:30:53 +0200

    Bug fix: Correct component namespace for TwoColumnBlocks

--- resources/views/components/blocks/two-column-block.blade.php
+++ resources/views/components/blocks/two-column-block.blade.php
@@ -2,14 +2,14 @@
     class="relative left-1/2 right-1/2 -mx-[50vw] w-screen my-12 lg:my-20"
     @if($backgroundColor) style="background-color: {{ $backgroundColor }}; padding: 80px 0;" @endif
 >
-    <div class="container">
+    <div class="container mx-auto">
         <div @class(['grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-20', 'items-center' => $centerContent])>
             <div
                 @class([$layoutRatio['left'], 'prose-content', 'items-center' => $centerContent, 'space-y-8 lg:space-y-12'])
                 @if($textStyle) style="{{ $textStyle }}" @endif
             >
                 @forelse ($leftColumn as $block)
-                    <x-dynamic-component :component="'blocks.' . $block->type" :data="$block->data"/>
+                    <x-dynamic-component :component="'crown-cms::blocks.' . $block->type" :data="$block->data"/>
                 @empty
                     {{-- Empty column --}}
                 @endforelse
@@ -20,7 +20,7 @@
                 @if($textStyle) style="{{ $textStyle }}" @endif
             >
                 @forelse ($rightColumn as $block)
-                    <x-dynamic-component :component="'blocks.' . $block->type" :data="$block->data"/>
+                    <x-dynamic-component :component="'crown-cms::blocks.' . $block->type" :data="$block->data"/>
                 @empty
                     {{-- Empty column --}}
                 @endforelse
